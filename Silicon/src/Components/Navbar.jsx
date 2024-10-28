3a514321c7a6638a6347ebd861fc158e537532f2
unknown <emeliefaldt@hotmail.com>
2024-10-28 17:17:05 +0100

    added images in assets folder and updated code for navbar

--- Silicon/src/Components/Navbar.jsx
+++ Silicon/src/Components/Navbar.jsx
@@ -1,6 +1,34 @@
 const Navbar = () => {
 
-    return
+    return(
+        <>
+            <nav id="main-menu" className="navbar">   
+                <div className="container">
+                    <a  id="logo" href="index.html">
+                        <img src="/HTML-assignment/Assets/silicon-logo.svg" alt="silicon logo"/>
+                    </a>     
+                       
+                            <a className="nav-link" href="#">Features</a>
+                        
+                        <div id="darkmode-toggle" className="btn-toggle-switch">
+                            <p>Dark Mode</p>
+                            <div className="toggle-switch">
+                                <input type="checkbox" id="darkmode-switch" className="toggle-input"/>
+                                <label for="darkmode-switch" className="slider"></label>
+                            </div>
+                        </div>
+                
+                            <a id="signin" href="#" className="btn btn-primary">
+                                <i class="fa-regular fa-user"></i>
+                                <span>Sign in / up</span>
+                            </a>
+                        </div>
+                
+                    
+                
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
